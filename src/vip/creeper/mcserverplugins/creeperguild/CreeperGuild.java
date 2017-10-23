db8b27d9c1b33bff8014152bb02d6b077011ddc7
July_ss <july_ss_2016@outlook.com>
2017-10-23 16:07:21 +0800

    create

---
+++ src/vip/creeper/mcserverplugins/creeperguild/CreeperGuild.java
@@ -0,0 +1,9 @@
+package vip.creeper.mcserverplugins.creeperguild;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CreeperGuild extends JavaPlugin {
+    public void onEnable() {
+
+    }
+}
---
+++ src/vip/creeper/mcserverplugins/creeperguild/utils/MsgUtil.java
@@ -0,0 +1,7 @@
+package vip.creeper.mcserverplugins.creeperguild.utils;
+
+import vip.creeper.mcserverplugins.creeperguild.CreeperGuild;
+
+public class MsgUtil {
+    private static Logger logger = CreeperGuild
+}
